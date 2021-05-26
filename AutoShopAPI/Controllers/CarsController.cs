0a644791320f24ad0ee5d80945b4f9d6a4fdd23e
Ejenaka <59808480+Ejenaka@users.noreply.github.com>
2021-05-26 03:00:05 +0300

    add begining of home work 2: ASP.NET Core WEB API

---
+++ AutoShopAPI/Controllers/CarsController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
+using AutoShopAPI.Models;
+using AutoShopAPI.Repositories;
+
+namespace AutoShopAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarsController : ControllerBase
+    {
+        private readonly CarRepository _carDB = new CarRepository();
+        private readonly ILogger<Car> _logger;
+
+        public CarsController(ILogger<Car> logger)
+        {
+            _logger = logger;
+        }
+
+        // GET: api/<СarsController>
+        [HttpGet]
+        public IEnumerable<Car> Get()
+        {
+            return _carDB.GetAll();
+        }
+
+        // GET api/<СarsController>/5
+        [HttpGet("{id}")]
+        public Car Get(int id)
+        {
+            return _carDB.Get(id);
+        }
+
+        // POST api/<СarsController>
+        [HttpPost]
+        public void AddCar([FromBody]Car car)
+        {
+            if (ModelState.IsValid)
+            {
+                _carDB.Add(car);
+            }
+        }
+
+        // PUT api/<СarsController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, Car car)
+        {
+        }
+
+        // DELETE api/<СarsController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _carDB.Delete(id);
+        }
+    }
+}
---
+++ AutoShopAPI/Models/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoShopAPI.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public int Price { get; set; }
+        public double EngineVolume { get; set; }
+        public int Mileage { get; set; }
+        public string Transmission { get; set; }
+    }
+}
